7d243636c31b82a5cce4ae3de84b18c6bf38f6a6
Dhruv Patel <dhruvpatelcode@gmail.com>
2023-03-21 18:44:56 -0400

    Added routes to transaction, created incomeController

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,30 @@
-const express = require("express");
-const cors = require("cors");
-const { db } = require("./db/db");
+const express = require('express')
+const cors = require('cors')
+const { db } = require('./db/db')
+const {readdirSync} = require('fs')
 
 //assigning variable to use express
-const app = express();
+const app = express()
 
-require("dotenv").config();
+require('dotenv').config()
 
 //assigning Port from env file
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 //middleware
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+
+//testing server
+const server = () => {
+    db()
+    app.listen(PORT, () =>{
+        console.log('You are listening to port', PORT)
+    })
+}
+
+server()
